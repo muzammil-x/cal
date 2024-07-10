deb94a24900b90a887bde9f868b7ab2c939d201e
muzammil-x <m.muzammilx2@gmail.com>
2024-07-10 12:03:08 +0500

    calculator version 2

---
+++ src/components/Container.jsx
@@ -0,0 +1,38 @@
+import styles from "./Container.module.css";
+
+const Container = ({ onButtonClick }) => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "0",
+    "9",
+    "=",
+    ".",
+  ];
+
+  return (
+    <div className={styles.buttonContainer}>
+      {buttonNames.map((buttonName) => (
+        <button
+          className={styles.button}
+          onClick={() => onButtonClick(buttonName)}
+        >
+          {buttonName}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Container;
